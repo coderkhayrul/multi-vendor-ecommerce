00997bea826cd87db1783f70ce3766357662492a
coderkhayrul <coderkhayrul@gmail.com>
2022-06-06 12:40:48 +0600

    Isotop Product Search

--- resources/views/frontend/includes/popular-product.blade.php
+++ resources/views/frontend/includes/popular-product.blade.php
@@ -4,27 +4,17 @@
         <div class="section-title style-2 wow animate__animated animate__fadeIn">
             <h3>Popular Products</h3>
             <ul class="nav nav-tabs links" id="myTab" role="tablist">
+                @php
+                    $categories = App\Models\Category::limit(6)->get();
+                @endphp
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active" id="nav-tab-one" data-bs-toggle="tab" data-bs-target="#tab-one" type="button" role="tab" aria-controls="tab-one" aria-selected="true">All</button>
                 </li>
+                @foreach ($categories as $category)
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="nav-tab-two" data-bs-toggle="tab" data-bs-target="#tab-two" type="button" role="tab" aria-controls="tab-two" aria-selected="false">Milks & Dairies</button>
+                    <button class="nav-link" id="nav-tab-two" data-bs-toggle="tab" data-bs-target="#tab-two" type="button" role="tab" aria-controls="tab-two" aria-selected="false">{{$category->category_name}}</button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="nav-tab-three" data-bs-toggle="tab" data-bs-target="#tab-three" type="button" role="tab" aria-controls="tab-three" aria-selected="false">Coffes & Teas</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="nav-tab-four" data-bs-toggle="tab" data-bs-target="#tab-four" type="button" role="tab" aria-controls="tab-four" aria-selected="false">Pet Foods</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="nav-tab-five" data-bs-toggle="tab" data-bs-target="#tab-five" type="button" role="tab" aria-controls="tab-five" aria-selected="false">Meats</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="nav-tab-six" data-bs-toggle="tab" data-bs-target="#tab-six" type="button" role="tab" aria-controls="tab-six" aria-selected="false">Vegetables</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="nav-tab-seven" data-bs-toggle="tab" data-bs-target="#tab-seven" type="button" role="tab" aria-controls="tab-seven" aria-selected="false">Fruits</button>
-                </li>
+                @endforeach
             </ul>
         </div>
         <!--End nav-tabs-->
@@ -32,6 +22,7 @@
             $products = App\Models\Product::where('product_status', 1)->get();
         @endphp
         <div class="tab-content" id="myTabContent">
+
             <div class="tab-pane fade show active" id="tab-one" role="tabpanel" aria-labelledby="tab-one">
                 <div class="row product-grid-4">
                     @foreach ($products as $data)
@@ -49,9 +40,9 @@
                                     <a aria-label="Compare" class="action-btn" href="shop-compare.html"><i class="fi-rs-shuffle"></i></a>
                                     <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal" data-bs-target="#quickViewModal"><i class="fi-rs-eye"></i></a>
                                 </div>
-                                {{-- <div class="product-badges product-badges-position product-badges-mrg">
+                                <div class="product-badges product-badges-position product-badges-mrg">
                                     <span class="hot">Hot</span>
-                                </div> --}}
+                                </div>
                             </div>
                             <div class="product-content-wrap">
                                 <div class="product-category">
@@ -85,7 +76,8 @@
                 <!--End product-grid-4-->
             </div>
             <!--En tab one-->
-            <div class="tab-pane fade" id="tab-two" role="tabpanel" aria-labelledby="tab-two">
+
+            {{-- <div class="tab-pane fade" id="tab-two" role="tabpanel" aria-labelledby="tab-two">
                 <div class="row product-grid-4">
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                         <div class="product-cart-wrap mb-30">
@@ -2748,7 +2740,7 @@
                 </div>
                 <!--End product-grid-4-->
             </div>
-            <!--En tab seven-->
+            <!--En tab seven--> --}}
 
         </div>
         <!--End tab-content-->
